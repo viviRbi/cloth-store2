ebee1a127e40069a369183ec6450336238cd70c3
ALDO_M <aldo.marukot@gmail.com>
2020-02-11 16:51:29 -0500

    aldos changes before the merge conflict debacle

--- cloth/views.py
+++ cloth/views.py
@@ -1,34 +1,49 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Category
 from .product_form import ProductForm
 
+
 def product_list(request):
-    products= Product.objects.all()
+    products = Product.objects.all()
     return render(request, 'cloth/product_list.html', {'products': products})
 
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'cloth/category_list.html', {'categories': categories})
+
+
 def product_detail(request, id):
-    product = Product.objects.get(id =id)
+    product = Product.objects.get(id=id)
     return render(request, 'cloth/product_detail.html', {'product': product})
+
+
+def category_detail(request, id):
+    category = Category.objects.get(id=id)
+    return render(request, 'cloth/category_detail.html', {'category': category})
+
 
 def product_create(request):
     if request.method == 'POST':
         form = ProductForm(request.POST)
         if form.is_valid():
             product = form.save()
-            return redirect('product_detail', id= product.id)
+            return redirect('product_detail', id=product.id)
     else:
         form = ProductForm()
-    return render(request, 'cloth/product_form.html',{'form':form})
+    return render(request, 'cloth/product_form.html', {'form': form})
+
 
 def product_edit(request, id):
     if request.method == 'POST':
         form = ProductForm(request.POST)
         if form.is_valid():
-            product.form =save()
+            product.form = save()
             return redirect('product_list')
-    else: 
+    else:
         form = ProductForm()
-    return render(request, 'cloth/product_form.html', {'form':form})
+    return render(request, 'cloth/product_form.html', {'form': form})
+
 
 def product_delete(request, id):
     Product.objects.get(id=id).delete()
