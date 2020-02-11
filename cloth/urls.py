dc067c5c7bcf1d6f2113e54cf93f106c9d9f164c
VyLe <thaovyletruong@gmail.com>
2020-02-11 11:59:11 -0600

    update

--- cloth/urls.py
+++ cloth/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from , import views
 
 urlpatterns =[
-    path('', views.product_list, name='product_list')
+    path('', views.product_list, name='product_list'),
+    path('')
 ]
