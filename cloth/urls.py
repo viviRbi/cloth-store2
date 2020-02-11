7d1d180db7be132253bb90e091be17937de897c7
VyLe <thaovyletruong@gmail.com>
2020-02-11 13:55:06 -0600

    finished create

--- cloth/urls.py
+++ cloth/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns =[
     path('', views.product_list, name='product_list'),
-    path('product/<int:id>/', views.product_detail, name='product_detail')
-]
+    path('product/<int:id>/', views.product_detail, name='product_detail'),
+    path('product/create', views.product_create, name='product_create')
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+## To display the images from static/ media folder
--- cloth/views.py
+++ cloth/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product
+from .product_form import ProductForm
 
 def product_list(request):
     products= Product.objects.all()
@@ -8,3 +9,13 @@ def product_list(request):
 def product_detail(request, id):
     product = Product.objects.get(id =id)
     return render(request, 'cloth/product_detail.html', {'product': product})
+
+def product_create(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            product = form.save()
+            return redirect('product_detail', id= product.id)
+    else:
+        form = ProductForm()
+    return render(request, 'cloth/product_form.html',{'form':form})
