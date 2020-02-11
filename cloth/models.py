60cdd6b6f822cf9611046ee3149d0c3cf515aff3
VyLe <thaovyletruong@gmail.com>
2020-02-11 10:46:59 -0600

    finished models

---
+++ cloth/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Category(models.Model):
+    name= models.CharField(default='', max_length=25)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name=models.CharField(default='', max_length=100)
+    des=models.TextField(default='')
+    price=models.DecimalField(max_digits=10, decimal_places=2)
+    quantity=models.IntegerField()
+    img=models.ImageField(default='', upload_to='cloth_product', unique=True, blank=True)
+    created=models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, default='', on_delete= models.CASCADE,related_name='products')
+
+    def __str__(self):
+        return self.name + self.category
