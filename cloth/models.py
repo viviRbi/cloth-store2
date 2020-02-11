c1d181a549d6d697984ed0c7c18a53c062043062
VyLe <thaovyletruong@gmail.com>
2020-02-11 11:54:13 -0600

    views and url for products

--- cloth/models.py
+++ cloth/models.py
@@ -11,7 +11,9 @@ class Product(models.Model):
     des=models.TextField(default='')
     price=models.DecimalField(max_digits=10, decimal_places=2)
     quantity=models.IntegerField()
+    # to upload image using Pillow  python -m pip install Pillow
     img=models.ImageField(default='', upload_to='cloth_product', unique=True, blank=True)
+    # created date
     created=models.DateTimeField(auto_now=True)
     category = models.ForeignKey(Category, default='', on_delete= models.CASCADE,related_name='products')
 
--- cloth/views.py
+++ cloth/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def product_list(request):
+    products= Product.objects.all()
+    return render(request, 'cloth/product_list.html', {'products': products})
+
+def product_detail(request, id):
+    product = Product.objects.get(id =id)
+    return render(request, 'cloth/product_detail.html', {'product': product})
